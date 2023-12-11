7915e628847d8845b45f7304bc9e636182d460f9
manoj <manojsinghsaun@gmail.com>
2023-12-11 16:30:04 +0530

    firs

---
+++ src/main/java/com/swissre/MvcGateApplication.java
@@ -0,0 +1,22 @@
+package com.swissre;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.client.RestClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
+
+
+
+
+@SpringBootApplication
+@AutoConfiguration(after = { RestTemplateAutoConfiguration.class, RestClientAutoConfiguration.class })
+public class MvcGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcGateApplication.class, args);
+	}
+  
+
+}
